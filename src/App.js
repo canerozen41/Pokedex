75d6ec2570a1c8666658645fc58b26638227e98d
Caner ÖZEN <canerozen410@gmail.com>
2024-08-17 20:56:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './Pokecard.css';
+import Pokegame from './Pokegame';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Pokemon Go Card Game</h1>
+            <Pokegame />
+        </div>
+    );
 }
 
 export default App;
